f360a4a610a167ec7899ad0fc293646bf497980a
Bekim <bekimrexhepi13@gmail.com>
2019-12-14 16:11:06 +0100

    Updates

---
+++ precache-manifest.d036334c7126c27e0040a78c70e20987.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74832ffbb2f0717e195105b2b00979ab",
+    "url": "/Movies-Web/index.html"
+  },
+  {
+    "revision": "666acb1c303f428701bf",
+    "url": "/Movies-Web/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "e05928b4133c46f2095a",
+    "url": "/Movies-Web/static/css/main.515d6be3.chunk.css"
+  },
+  {
+    "revision": "666acb1c303f428701bf",
+    "url": "/Movies-Web/static/js/2.6bdcf019.chunk.js"
+  },
+  {
+    "revision": "e05928b4133c46f2095a",
+    "url": "/Movies-Web/static/js/main.70865614.chunk.js"
+  },
+  {
+    "revision": "29c6792980d74c1f7a7f",
+    "url": "/Movies-Web/static/js/runtime~main.385d27af.js"
+  },
+  {
+    "revision": "b6d4939b8ce8b3809ded0447078eb143",
+    "url": "/Movies-Web/static/media/star-review-no-bg.b6d4939b.png"
+  }
+]);
